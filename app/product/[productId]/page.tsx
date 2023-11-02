3e7bb3b6db694fde9f50221593994a7a6811bc4b
Ankit Sharma <ankitsharma290101@gmail.com>
2023-11-02 07:17:13 +0530

    Each Product Page + Product Details

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,12 +1,19 @@
+import Container from "@/app/components/Container";
+import { product } from "@/utils/product";
+import ProductDetails from "./ProductDetails";
 
 interface Iparams{
     productId:string
 }
 const ProductPage = ({params}:{params:Iparams}) => {
     console.log("params",params)
+    
     return (
-        <div>
-            ProductPage
+        <div className="p-8">
+            <Container>
+                <ProductDetails product={product} />
+            </Container>
+    
         </div>
      );
 }
