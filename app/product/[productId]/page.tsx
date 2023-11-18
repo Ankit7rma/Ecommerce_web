054714026bb543a8ae212d6b5d5737aebaaad18b
Ankit Sharma <ankitsharma290101@gmail.com>
2023-11-18 23:01:49 +0530

    Product Ratings

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,21 +1,25 @@
 import Container from "@/app/components/Container";
 import { product } from "@/utils/product";
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRatings";
 
-interface Iparams{
-    productId:string
+interface Iparams {
+  productId: string;
 }
-const ProductPage = ({params}:{params:Iparams}) => {
-    console.log("params",params)
-    
-    return (
-        <div className="p-8">
-            <Container>
-                <ProductDetails product={product} />
-            </Container>
-    
+const ProductPage = ({ params }: { params: Iparams }) => {
+  console.log("params", params);
+
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetails product={product} />
+        <div className="flex flex-col mt-20 gap-4">
+          <div>Add Rating</div>
+          <ListRating product={product} />
         </div>
-     );
-}
- 
+      </Container>
+    </div>
+  );
+};
+
 export default ProductPage;
